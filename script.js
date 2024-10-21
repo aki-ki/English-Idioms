25105aae617d70f96b12ac72339060f6fb97f7f9
aki <a.smith69@share.epsb.ca>
2024-10-21 14:44:59 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("bark").addEventListener("click", barking);
+function barking() {
+  alert("tree");
+}
+
+document.getElementById("penny").addEventListener("click", pennies);
+function pennies() {
+  alert("thoughts");
+}
+
+document.getElementById("break").addEventListener("click", breakk);
+function breakk() {
+  alert("leg");
+}
+
+document.getElementById("benefit").addEventListener("click", benefits);
+function benefits() {
+  alert("doubt");
+}
+
+document.getElementById("beat").addEventListener("click", clickk);
+function clickk() {
+  alert("bush");
+}
